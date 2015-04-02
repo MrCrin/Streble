052cf1e277a36427f29e0b845152faa7ed9035e1
Michael Crinnion <crin@crin.co.uk>
2015-04-02 12:05:59 +0100

    StravaWatch 1.0
    This is a working Pebble watchface created using SDK 2.0. There are no configuration options at present and it relies on a PHP script not included in this repo. If you wish to use it then contact me and I will send you the PHP script used to scrape stats from a Strava public profile page.

---
+++ src/main.c
@@ -0,0 +1,254 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
+static TextLayer *s_month_layer;
+static TextLayer *s_totals_layer;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+static TextLayer *s_monthmiles_layer;
+static TextLayer *s_monthelevation_layer;
+static TextLayer *s_totalmiles_layer;
+static TextLayer *s_totalelevation_layer;
+
+
+enum {
+  KEY_MONTHMILES = 0,
+  KEY_MONTHELEVATION = 1,
+  KEY_TOTALMILES = 2,
+  KEY_TOTALELEVATION = 3,
+};
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create time and date buffer
+  static char buffer[] = "00:00";
+  static char datebuffer[] = "WEDNESDAY 00";
+	 static char monthbuffer[] = "SEPTEMBER TOTALS";	
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    // Use 24 hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    // Use 12 hour format
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+	
+		//Set date and display on date layer
+		strftime(datebuffer, sizeof(datebuffer), "%A %d", tick_time);
+		text_layer_set_text(s_date_layer, datebuffer);
+	
+		//Set month and display on month layer
+		strftime(monthbuffer, sizeof(monthbuffer), "%B Totals", tick_time);
+		text_layer_set_text(s_month_layer, monthbuffer);
+	
+  // Display this time on time layer
+  text_layer_set_text(s_time_layer, buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+	  update_time();
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  (void) context;
+	 static char monthmiles_buffer[32];
+	 static char monthelevation_buffer[32];
+		static char totalmiles_buffer[32];
+		static char totalelevation_buffer[32];
+
+	 void process_tuple(Tuple *t) {
+		
+  //Get key
+  int key = t->key;
+ 
+  //Get string value
+  char string_value[32];
+  strcpy(string_value, t->value->cstring);
+ 
+  //Decide what to do
+  switch(key) {
+    case KEY_MONTHMILES:
+      snprintf(monthmiles_buffer, sizeof("000000 mi"), "%s mi", string_value);
+      text_layer_set_text(s_monthmiles_layer, monthmiles_buffer);
+			 case KEY_MONTHELEVATION:
+      snprintf(monthelevation_buffer, sizeof("ft 000000"), "ft %s", string_value);
+      text_layer_set_text(s_monthelevation_layer, monthelevation_buffer);
+			 case KEY_TOTALMILES:
+      snprintf(totalmiles_buffer, sizeof("000000 mi"), "%s mi", string_value);
+      text_layer_set_text(s_totalmiles_layer, totalmiles_buffer);
+    case KEY_TOTALELEVATION:
+      snprintf(totalelevation_buffer, sizeof("ft 000000"), "ft %s", string_value);
+      text_layer_set_text(s_totalelevation_layer, totalelevation_buffer);
+
+      break;
+		}
+}
+     
+  //Get data
+  Tuple *t = dict_read_first(iterator);
+  while(t != NULL)
+    {
+      process_tuple(t);
+      //Get next
+      t = dict_read_next(iterator);
+    }
+  
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+static void main_window_load(Window *window) {
+  // Create GBitmap, then set to created BitmapLayer
+	 s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BG_IMAGE);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));	
+	
+		// Create time layer
+  s_time_layer = text_layer_create(GRect(-2, 52, 146, 52));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+	 layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+	
+		// Create date layer
+  s_date_layer = text_layer_create(GRect(-2, 103, 146, 22));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+	 layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+	 text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+	
+		// Create month layer
+  s_month_layer = text_layer_create(GRect(-2, 125, 146, 22));
+  text_layer_set_background_color(s_month_layer, GColorClear);
+  text_layer_set_text_color(s_month_layer, GColorWhite);
+  text_layer_set_text_alignment(s_month_layer, GTextAlignmentCenter);
+	 layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_month_layer));
+	 text_layer_set_font(s_month_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+	
+		// Create totals layer
+  s_totals_layer = text_layer_create(GRect(-2, 17, 146, 22));
+  text_layer_set_background_color(s_totals_layer, GColorClear);
+  text_layer_set_text_color(s_totals_layer, GColorWhite);
+  text_layer_set_text_alignment(s_totals_layer, GTextAlignmentCenter);
+	 layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_totals_layer));
+	 text_layer_set_font(s_totals_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+	 text_layer_set_text(s_totals_layer, "All-Time Totals");
+
+	 //Create and apply custom time font
+	 s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_THEBOLDFONT_REGULAR_52));
+  text_layer_set_font(s_time_layer, s_time_font);
+		
+	 // Make sure the time is displayed from the start
+	 update_time();
+	
+  // Create monthly miles Layer
+  s_monthmiles_layer = text_layer_create(GRect(0, 144, 67, 25));
+  text_layer_set_background_color(s_monthmiles_layer, GColorClear);
+  text_layer_set_text_color(s_monthmiles_layer, GColorWhite);
+  text_layer_set_text_alignment(s_monthmiles_layer, GTextAlignmentRight);
+  text_layer_set_text(s_monthmiles_layer, "-");
+	 text_layer_set_font(s_monthmiles_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_monthmiles_layer));
+	
+  // Create monthly elevation Layer
+  s_monthelevation_layer = text_layer_create(GRect(76, 144, 65, 25));
+  text_layer_set_background_color(s_monthelevation_layer, GColorClear);
+  text_layer_set_text_color(s_monthelevation_layer, GColorWhite);
+  text_layer_set_text_alignment(s_monthelevation_layer, GTextAlignmentLeft);
+  text_layer_set_text(s_monthelevation_layer, "-");
+	 text_layer_set_font(s_monthelevation_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_monthelevation_layer));
+	
+  // Create total miles Layer
+  s_totalmiles_layer = text_layer_create(GRect(0, -1, 67, 25));
+  text_layer_set_background_color(s_totalmiles_layer, GColorClear);
+  text_layer_set_text_color(s_totalmiles_layer, GColorWhite);
+  text_layer_set_text_alignment(s_totalmiles_layer, GTextAlignmentRight);
+  text_layer_set_text(s_totalmiles_layer, "-");
+	 text_layer_set_font(s_totalmiles_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_totalmiles_layer));
+	
+  // Create total elevation Layer
+  s_totalelevation_layer = text_layer_create(GRect(76, -1, 65, 25));
+  text_layer_set_background_color(s_totalelevation_layer, GColorClear);
+  text_layer_set_text_color(s_totalelevation_layer, GColorWhite);
+  text_layer_set_text_alignment(s_totalelevation_layer, GTextAlignmentLeft);
+  text_layer_set_text(s_totalelevation_layer, "-");
+	 text_layer_set_font(s_totalelevation_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_totalelevation_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+		text_layer_destroy(s_time_layer);
+	
+  // Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
+	
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_background_layer);
+	
+	 // Destroy stats elements
+  text_layer_destroy(s_monthmiles_layer);
+  text_layer_destroy(s_monthelevation_layer);
+  text_layer_destroy(s_totalmiles_layer);
+  text_layer_destroy(s_totalelevation_layer);
+}
+
+static void init() {
+		// Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+	
+	 // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+	 // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+	
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
+}
+
+static void deinit() {
+		// Destroy Window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
+
---
+++ src/stravaAPI.js
@@ -0,0 +1,75 @@
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function getStats() {
+  // Construct URL
+  var url = "http://crin.co.uk/stravaWatchface/getStats.php";
+
+  // Send request to crin.co.uk
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      // responseText contains a JSON object with stat info
+      var json = JSON.parse(responseText);
+
+      // Monthly miles
+      var monthMiles = json.mM;
+					console.log("Monthly total miles is: " + monthMiles);
+
+      // Monthly Elevation
+      var monthElevation = json.mE;      
+      console.log("Monthly total elevation gain is: " + monthElevation);
+
+      // Total Miles
+      var totalMiles = json.tM;      
+      console.log("All-time total miles is: " + totalMiles);
+
+      // Total Elevation
+      var totalElevation = json.tE;      
+      console.log("All-time total elevation gain is: " + totalElevation);
+      
+      // Assemble dictionary using our keys
+      var dictionary = {
+       "KEY_MONTHMILES": monthMiles,
+       "KEY_MONTHELEVATION": monthElevation,
+       "KEY_TOTALMILES": totalMiles,
+							"KEY_TOTALELEVATION": totalElevation
+						};
+
+      // Send to Pebble
+      Pebble.sendAppMessage(dictionary,
+        function(e) {
+          console.log("Stats info sent to Pebble successfully!");
+        },
+        function(e) {
+          console.log("Error sending stats info to Pebble!");
+        }
+      );
+    }      
+  );
+}
+
+
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log("PebbleKit JS ready!");
+
+    // Get the initial weather
+    getStats();
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log("AppMessage received!");
+    getStats();
+  }                     
+);
